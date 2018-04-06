c95c3968203d22e17bcec0687d3789ff31c3d43d
jennifer-dutra <jrdutra@ucsc.edu>
2018-04-05 22:25:46 -0700

    correctly parses input

--- Lab1/rpnc.py
+++ Lab1/rpnc.py
@@ -2,5 +2,24 @@
 # Python3 Reverse Polish Notation (RPN) Calculator
 #
 # Accepts an arbitrary number of arguments in RPN, evaluates them and prints the answer to stdout.
-# 
-print("not implemented")
+#
+
+import sys
+
+def main():
+
+    input = sys.argv[1]
+
+
+    for i in input:
+        print(i)
+        if i == '+' or i == '-' or i == '*' or i == '/' or i == '^':
+            print("operator, ")
+        elif i.isdigit():
+            print("is digit, ")
+        else:
+            print("space, ")
+
+
+if __name__ == "__main__":
+    main()
