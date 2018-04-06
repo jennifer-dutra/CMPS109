aeeff382e299649ff60673ae6acdc4efb51be2b7
jennifer-dutra <jrdutra@ucsc.edu>
2018-04-05 17:40:44 -0700

    edit makefile libraries, changed stack implementation

--- Lab1/rpnc.c
+++ Lab1/rpnc.c
@@ -12,109 +12,100 @@
  * Returns -1 on error, 0 otherwise.
  */
 
+
 /*
- * STACK INTERFACE - temporary
- * Author: Robert I. Pitts <rip@cs.bu.edu>
- *
+ * Array based implementation of stack
+ * Referenced:
+ * https://www.cs.bu.edu/teaching/c/stack/array/
  */
 
-# ifndef _STACK_H
-# define _STACK_H
-
-typedef int stackElementT;
+typedef int stackElement;
 
 typedef struct {
- stackElementT *contents;
- int maxSize;
- int top;
-} stackT;
+ stackElement *array;   // pointer to contents of the array
+ int capacity;          // max size of array
+ int top;               // represents current size of stack
+} stack;
 
-void StackInit(stackT *stackP, int maxSize);
+void createStack(stack *sp, int capacity);        // make stack, allocate memory
 
-void StackDestroy(stackT *stackP);
+void deleteStack(stack *sp);                      // delete stack, free memory
 
-void StackPush(stackT *stackP, stackElementT element);
+void push(stack *sp, stackElement num);           // push number to stack
 
-stackElementT StackPop(stackT *stackP);
+stackElement pop(stack *sp);                      // pop top element off stack
 
-int StackIsEmpty(stackT *stackP);
-
-int StackIsFull(stackT *stackP);
-
-#endif  /* not defined _STACK_H */
-
-int operate(int num1, int num2, char operation); // completes operation
+int operate(int num1, int num2, char operation);  // completes operation
 
 /*
  * MAIN - RPN calculator
- *
  */
 
 int main(int argc, char *argv[]) {
+  char *rpn = argv[1];                  // pointer to RPN argument
+  stack rpnStack;                       // declare stack
+  createStack(&rpnStack, strlen(rpn));  // create stack, allocate space for all nums
+  int sum = 0;                          // current sum to push to stack
+  int result = 0;                       // final calculation to return
 
-  char *rpn = argv[1];            // pointer to RPN argument
-  stackT stack;                   // declare stack
-  StackInit(&stack, strlen(rpn)); // create stack, allocate space
-  int sum = 0;                    // current sum to push to stack
-  int result = 0;                 // final calculation to return
-
-  // parse string character by character until end of string is reached
-  // evaluates during loop and returns final sum (at the top of the stack)
+  /* Parse RPN string character by character until end of string is reached.
+  Evaluates RPN during loop and returns final sum (at the top of the stack) */
   for(int i = 0; rpn[i] != '\0'; i++) {
     int currNum = 0; // current number
 
-    // if character is a digit
+    /* if character is a digit */
     while(isdigit(rpn[i])) {
-      // convert char to int and add digit to current number
-      // must shift the current number by power of 10 (ex. '55' = '5' + '5' = 50 + 5)
+      /* Convert char to int and add digit to current number.
+         Must shift the current number by power of 10 (ex. '55' = '5' + '5' = 50 + 5) */
       currNum = (currNum * 10) + (rpn[i] - '0');
 
-      // if the next digit is not a part of the same number, push number to the stack
+      /* If the next digit is not a part of the same number, push number to the stack */
       if(!isdigit(rpn[i + 1])) {
-        StackPush(&stack, currNum);
+        push(&rpnStack, currNum);
       }
       i++;  // iterate to the next character in the string
     }
 
-    // if character is an operation
+    /* if character is an operation */
     if(rpn[i] == '+' || rpn[i] == '-' || rpn[i] == '*' || rpn[i] == '/' || rpn[i] == '^') {
-      int num1 = StackPop(&stack);        // pop first number from stack
-      int num2 = StackPop(&stack);        // pop second number from stack
-      sum = operate(num1, num2, rpn[i]);  // perform desired operation on two integers
-      StackPush(&stack, sum);             // update current sum by pushing to the stack
+      int num1 = pop(&rpnStack);             // pop first number from stack
+      int num2 = pop(&rpnStack);             // pop second number from stack
+      sum = operate(num1, num2, rpn[i]);     // perform desired operation on two integers
+      push(&rpnStack, sum);                  // update current sum by pushing to the stack
     }
   }
 
-  // final sum is at the top of stack
-  if(!StackIsEmpty(&stack)) {
-    result = StackPop(&stack);
-    printf("%d", result);
-    return 0;
-  }
+  /* final sum is at the top of stack */
+  result = pop(&rpnStack);
+  printf("%d", result);
+  deleteStack(&rpnStack);
+  return 0;
 
   return -1; // return -1 if there is there is an error
-}
+  }
 
 
 /*
  * OPERATE - when program reaches operator in string, this
  * function is called to peform the correct arithmetic.
+ * Handles addition, subtraction, multiplication, division, and powers.
+ *
  */
 int operate(int num1, int num2, char operation) {
   if(operation == '+') {
-    return (num2 + num1);        // addition
+    return (num2 + num1);
   }
   else if (operation == '-') {
-    return (num2 - num1);        // subtraction
+    return (num2 - num1);
   }
   else if (operation == '*') {
-    return (num2 * num1);        // multiplication
+    return (num2 * num1);
   }
   else if (operation == '/') {
-    return (num2 / num1);        // division
+    return (num2 / num1);
   }
   else if (operation == '^') {
-    return (pow(num2, num1));    // power
+    return (pow(num2, num1));
   }
   else { // CHANGE THIS??
     return 0;
@@ -123,67 +114,38 @@ int operate(int num1, int num2, char operation) {
 
 
 /*
- * STACK IMPLEMENTATION - temporary
- * Author: Robert I. Pitts <rip@cs.bu.edu>
+ * Stack Implementation
+ * Referenced: https://www.cs.bu.edu/teaching/c/stack/array
  *
 */
 
-void StackInit(stackT *stackP, int maxSize)
+/* Creates a stack and allocates memory accordingly, updates pointers */
+void createStack(stack *sp, int capacity)
 {
-  stackElementT *newContents;
-
-  /* Allocate a new array to hold the contents. */
-
-  newContents = (stackElementT *)malloc(sizeof(stackElementT) * maxSize);
-
-  if (newContents == NULL) {
-    fprintf(stderr, "Insufficient memory to initialize stack.\n");
-    exit(1);  /* Exit, returning error code. */
-  }
-
-  stackP->contents = newContents;
-  stackP->maxSize = maxSize;
-  stackP->top = -1;  /* I.e., empty */
+  /* Create stack pointer and allocate memory */
+  stackElement *newarray;
+  newarray = (stackElement *)malloc(sizeof(stackElement) * capacity);
+  sp->array = newarray;     // set new stack pointer
+  sp->capacity = capacity;  // set capacity
+  sp->top = -1;             // the array is empty so top = -1
 }
 
-void StackDestroy(stackT *stackP)
+/* Delete stack and free associated memory */
+void deleteStack(stack *sp)
 {
-  /* Get rid of array. */
-  free(stackP->contents);
-
-  stackP->contents = NULL;
-  stackP->maxSize = 0;
-  stackP->top = -1;  /* I.e., empty */
+  free(sp->array);
+  sp->array = NULL;
+  sp->capacity = 0;
+  sp->top = -1;
 }
 
-void StackPush(stackT *stackP, stackElementT element)
+/* Push element to the stack */
+void push(stack *sp, stackElement element)
 {
-  if (StackIsFull(stackP)) {
-    fprintf(stderr, "Can't push element on stack: stack is full.\n");
-    exit(1);  /* Exit, returning error code. */
-  }
-
-  /* Put information in array; update top. */
-
-  stackP->contents[++stackP->top] = element;
+  sp->array[++sp->top] = element;
 }
 
-stackElementT StackPop(stackT *stackP)
-{
-  if (StackIsEmpty(stackP)) {
-    fprintf(stderr, "Can't pop element from stack: stack is empty.\n");
-    exit(1);  /* Exit, returning error code. */
-  }
-
-  return stackP->contents[stackP->top--];
-}
-
-int StackIsEmpty(stackT *stackP)
-{
-  return stackP->top < 0;
-}
-
-int StackIsFull(stackT *stackP)
-{
-  return stackP->top >= stackP->maxSize - 1;
+/* Pop element off the stack */
+stackElement pop(stack *sp) {
+  return sp->array[sp->top--];
 }
