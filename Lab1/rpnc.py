f00b3b04048e7f190eb1c9a5b85511dc2309dc71
jennifer-dutra <jrdutra@ucsc.edu>
2018-04-06 17:06:15 -0700

    python implementation works for single digit

--- Lab1/rpnc.py
+++ Lab1/rpnc.py
@@ -10,34 +10,37 @@ def main():
 
     input = sys.argv[1]
     currNum = 0
+    stack = []
 
     for i in input:
         print(i)
         if i.isdigit():
             digit = ord(i) - 48
-            currNum = (currNum * 10) + digit
-            print("push digit: %d" % digit)
+            # currNum = (currNum * 10) + digit
+            stack.append(digit)
+            print(stack)
 
+        elif i == '+' or i == '-' or i == '*' or i == '/' or i == '^':
+            print("operator %c, " % i)
+            num1 = stack[-1]
+            stack.pop()
+            num2 = stack[-1]
+            stack.pop()
+            result = operation(num1, num2, i)
+            stack.append(result)
+            print(stack[-1])
 
-        if i == '+' or i == '-' or i == '*' or i == '/' or i == '^':
-            print("operator, ")
-            # pop, pop
-            # call operation
-            # push back updated val
-        else:
-            print("space, ")
-
-# def operation(num1, num2, operator):
-#     if operator == '+':
-#         return num2 + num1
-#     elif operator == '-':
-#         return num2 - num1
-#     elif operator == '*':
-#         return num2 * num1
-#     elif operator == '/':
-#         return num2 / num1
-#     elif operator == '^':
-        # return num2 ^ num1
+def operation(num1, num2, operator):
+    if operator == '+':
+        return num2 + num1
+    elif operator == '-':
+        return num2 - num1
+    elif operator == '*':
+        return num2 * num1
+    elif operator == '/':
+        return num2 / num1
+    elif operator == '^':
+        return pow(num2, num1)
 
 if __name__ == "__main__":
     main()
