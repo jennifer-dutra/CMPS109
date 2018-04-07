db04806d030f30d048c8fbf9fbb4f89078c1f5ee
jennifer-dutra <jrdutra@ucsc.edu>
2018-04-07 14:35:09 -0700

    bash: addition working

--- Lab1/rpnc.sh
+++ Lab1/rpnc.sh
@@ -2,5 +2,33 @@
 # Bash Reverse Polish Notation (RPN) Calculator
 #
 # Accepts an arbitrary number of arguments in RPN, evaluates them and prints the answer to stdout.
-# 
-echo "not implemented"
+#
+
+# echo $1
+
+stack=()
+top=0     # next empty spot in stack
+
+for oper in $1
+do
+  if [[ $oper =~ ^[0-9]+$ ]]; then
+    # echo "push: $oper, "
+    stack[$top]=$oper
+    (( top += 1 ))
+  elif [ "$oper" == '+' ]; then
+    # echo "plus"
+    (( num1_idx = top - 2 ))
+    (( num2_idx = top - 1 ))
+    (( top -= 2))
+    num1=${stack[num1_idx]}
+    num2=${stack[num2_idx]}
+    (( result = num1 + num2 ))
+    (( top += 1 ))
+    # echo "pop: $num1, "
+    # echo "pop: $num2, "
+
+  fi
+  # echo $oper
+done
+
+echo $result
