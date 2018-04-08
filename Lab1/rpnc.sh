6dc7d533d7fa58b1ec1f6dd7b3745b9da2321b28
jennifer-dutra <jrdutra@ucsc.edu>
2018-04-07 18:15:29 -0700

    bash complete, comments added

--- Lab1/rpnc.sh
+++ Lab1/rpnc.sh
@@ -3,37 +3,41 @@
 #
 # Accepts an arbitrary number of arguments in RPN, evaluates them and prints the answer to stdout.
 #
+# Based Prof. Harrison's in class example:
+# Source: https://opencast-player-1.lt.ucsc.edu:8443/engage/theodul/ui/core.html?id=764d4b0f-c245-4492-a317-4dded677a139
+#
 
-stack=()
-top=0     # next empty spot in stack
+input=`echo "$1" | sed 's/\*/x/g'`  # replace all '*' with 'x'
+sp=0                                # next empty spot in stack (stack pointer)
+stack=()                            # array implementation of stack
 
-for oper in $1
+# loop through all tokens in the rpn argument
+for oper in $input
 do
+  # if the string contains only digits, push to stack
   if [[ $oper =~ ^[0-9]+$ ]]; then
-    # echo "push: $oper, "
-    stack[$top]=$oper
+    stack[$sp]=$oper
+  # if the string is an operator
   else
-    (( num1_idx = top - 2 ))
-    (( num2_idx = top - 1 ))
-    (( top -= 2))
-    num1=${stack[num1_idx]}
+    (( num1_idx = sp - 2 )) # store index of second to top element
+    (( num2_idx = sp - 1 )) # store index of top element
+    (( sp -= 2))
+    num1=${stack[num1_idx]} # store top two numbers ("pop" off stack)
     num2=${stack[num2_idx]}
-    # echo "pop: $num2, "
-    # echo "pop: $num1, "
     if [ "$oper" == '+' ]; then
-      (( result = $num1 + $num2 ))
+      (( result = $num1 + $num2 ))  # addition
     elif [ "$oper" == '-' ]; then
-      (( result = $num1 - $num2 ))
+      (( result = $num1 - $num2 ))  # subtraction
     elif [ "$oper" == '/' ]; then
-      (( result = $num1 / $num2 ))
+      (( result = $num1 / $num2 ))  # divison
     elif [ "$oper" == '^' ]; then
-      (( result = $num1 ** $num2 ))
+      (( result = $num1 ** $num2 )) # power
+    elif [ "$oper" == 'x' ]; then
+      (( result = $num1 * $num2 ))  # multiplication
     fi
-    stack[$top]=$result
-    # echo "push: $result, "
+    stack[$sp]=$result  # store result of calculation at top of stack
   fi
-  (( top += 1 ))
-  # echo $oper
+  (( sp += 1 )) # new element, adjust stack pointer
 done
 
-echo ${stack[0]}
+echo ${stack[$top]}  # return result at the top of the stack
