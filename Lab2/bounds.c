f93fcc9aa1cc15e142ac725c4d9da4dbb1fd768a
jennifer-dutra <jrdutra@ucsc.edu>
2018-04-10 22:49:06 -0700

    passes all trivial tests

--- Lab2/bounds.c
+++ Lab2/bounds.c
@@ -1,23 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 #include "bounds.h"
 
 /*
- * Intialise a two or thre dimensional space of shape ARENA containing NUMSHAPES 
+ * Intialise a two or thre dimensional space of shape ARENA containing NUMSHAPES
  * SHAPES, all of which may or may not be entirely contained within ARENA.
  */
-void setup(Shape *arena, Shape *shapes[], int numShapes) {
-    int ununsed; // here to fail warnings test, remove it
+
+static Shape *arena;
+
+void setup(Shape *a, Shape *shapes[], int numShapes) {
+  arena = a;
 }
 
+static bool isCircleInsideCircle(Circle *outer, Circle *inner) {
+  float distance = sqrt (
+      pow(outer->center.x - inner->center.x,2) +
+      pow(outer->center.y - inner->center.y,2));
+      return distance <= outer->radius - inner->radius;
+}
+
+
 /*
- * Move SHAPE to POINT. 
+ * Move SHAPE to POINT.
  *
- * Return TRUE if SHAPE remains entirely within the space defined by the 
+ * Return TRUE if SHAPE remains entirely within the space defined by the
  * ARENA parameter of setup(), FALSE otherwise.
  */
 bool move(Shape *shape, Point *point) {
-    printf("Not implemented!\n");
-    exit(-1);
+  if(arena->type == CIRCLE && shape->type == CIRCLE) {
+    Circle *inner = (Circle *)shape;
+    inner->center.x = point->x;
+    inner->center.y = point->y;
+    return isCircleInsideCircle((Circle *)arena, (Circle *)shape );
+  }
+  return true;
 }
