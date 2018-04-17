bc997f2bbbcb2c3f30cd0caabd558f1b7f598967
jennifer-dutra <jrdutra@ucsc.edu>
2018-04-17 16:29:02 -0700

    implemented circle in square, triangle in circle, polygon contained within

--- Lab3/src/main.cc
+++ Lab3/src/main.cc
@@ -1,7 +1,7 @@
 /*
  * Copyright (C) 2018 David C. Harrison. All right reserved.
  *
- * You may not use, distribute, publish, or modify this code without 
+ * You may not use, distribute, publish, or modify this code without
  * the express written permission of the copyright holder.
  */
 
@@ -10,16 +10,37 @@
 #include "circle.h"
 #include "polygon.h"
 
-int main(int argc, char *argv[]) 
+static void result(const char *msg, bool expected, bool got) {
+  std::cout << msg;
+  if(expected == got)
+    std::cout << "PASS\n";
+  else
+    std::cout << "FAIL\n";
+}
+
+static void testCirclesInCircles() {
+  Circle inner = Circle(Point2D(0.0,0.0), 4.0);
+  Circle outer = Circle(Point2D(0.0,0.0), 8.0);
+  result("Circle-in-Circle: ", true, inner.containedWithin(outer));
+}
+
+static void testCirclesInSquares() {
+  Circle inner = Circle(Point2D(0.0,0.0), 4.0);
+  RegularConvexPolygon outer = RegularConvexPolygon({
+    Point2D(-12, -12), Point2D(-12,12), Point2D(12,12), Point2D(12,-12)});
+  result("Square-in-Circle: ", true, inner.containedWithin(outer));
+}
+
+static void testTrianglesInCircles() {
+  Circle outer = Circle(Point2D(0.0,0.0), 8.0);
+  RegularConvexPolygon inner = RegularConvexPolygon({
+    Point2D(-1, -1), Point2D(0,2), Point2D(1,-1)});
+  result("Triangle-in-Circle: ", true, inner.containedWithin(outer));
+}
+
+int main(int argc, char *argv[])
 {
-    int ununsed; // to fail warning test, remove
-
-    Circle inner = Circle(Point2D(0.0,0.0), 4.0);
-    Circle outer = Circle(Point2D(0.0,0.0), 8.0); 
-
-    std::cout << "Circle-in-Circle: ";
-    if (inner.containedWithin(outer)) 
-        std::cout << "PASS\n";
-    else 
-        std::cout << "FAIL\n";
+    testCirclesInCircles();
+    testCirclesInSquares();
+    testTrianglesInCircles();
 }
--- Lab3/src/polygon.cc
+++ Lab3/src/polygon.cc
@@ -1,13 +1,14 @@
 /*
  * Copyright (C) 2018 David C. Harrison. All right reserved.
  *
- * You may not use, distribute, publish, or modify this code without 
+ * You may not use, distribute, publish, or modify this code without
  * the express written permission of the copyright holder.
  */
 
 #include <iostream>
 
 #include "polygon.h"
+#include "geom.h"
 
 RegularConvexPolygon::RegularConvexPolygon(std::vector<Point2D> vertices) {
     vertices_ = vertices;
@@ -19,7 +20,7 @@ std::vector<Point2D> RegularConvexPolygon::vertices() {
 
 std::vector<Line> RegularConvexPolygon::edges() {
     std::vector<Line> edges;
-    for (unsigned int i = 0; i < vertices_.size()-1; i++) 
+    for (unsigned int i = 0; i < vertices_.size()-1; i++)
         edges.push_back(Line(vertices_[i],vertices_[i+1]));
     edges.push_back(Line(vertices_[vertices_.size()-1],vertices_[0]));
     return edges;
@@ -30,7 +31,12 @@ int RegularConvexPolygon::vertexCount() {
 }
 
 bool RegularConvexPolygon::containedWithin(Circle &circle) {
-    throw "Not implemented";
+    for(Point2D const &vertex: vertices()) {
+      if(Geom::distance(vertex, circle.center()) > circle.radius()) {
+        return false;
+      }
+    }
+    return true;
 }
 
 bool RegularConvexPolygon::containedWithin(RegularConvexPolygon &polygon) {
